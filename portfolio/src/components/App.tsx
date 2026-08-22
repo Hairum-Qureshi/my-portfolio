d95fcdb1f58b419df52cb22e786b90f375f54c1f
Hairum Qureshi <hqureshi@udel.edu>
2026-08-22 18:55:28 -0400

    turned App component into Routes component

--- portfolio/src/components/App.tsx
+++ portfolio/src/components/App.tsx
@@ -1,3 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
