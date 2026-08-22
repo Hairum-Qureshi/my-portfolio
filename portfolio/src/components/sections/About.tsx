7ecee8b175d01f7b17eb2d8b2966fcbd2e197720
Hairum Qureshi <hqureshi@udel.edu>
2026-08-22 19:22:39 -0400

    deleted history component

--- portfolio/src/components/sections/About.tsx
+++ portfolio/src/components/sections/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div id = "about" className="flex items-center justify-center h-screen bg-gradient-to-b from-gray-800 to-black text-white min-h-auto">
-        <h2>About</h2>
+        <h2>About & History</h2>
     </div>
   );
 }
--- portfolio/src/components/sections/History.tsx
+++
@@ -1,7 +0,0 @@
-export default function History() {
-  return (
-    <div id="history" className="flex items-center justify-center h-screen bg-gradient-to-b from-gray-800 to-black text-white min-h-auto">
-      <h2>History</h2>
-    </div>
-  );
-}
